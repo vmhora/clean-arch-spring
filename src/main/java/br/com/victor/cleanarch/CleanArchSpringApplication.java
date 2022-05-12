b418083ba2c4adfd7e45fd239d23a07e03ba7a3b
victor <vmshora@gmail.com>
2022-05-12 19:55:19 -0300

    primeiro commit

---
+++ src/main/java/br/com/victor/cleanarch/CleanArchSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor.cleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victor/cleanarch/CleanArchSpringApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.victor.cleanarch;
+
+import org.junit.jupiter.api.Test;
+
+class CleanArchSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
